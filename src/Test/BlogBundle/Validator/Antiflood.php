1726d4b4ea1cacfe19e80bdea44f5fe5a7026934
VirginieGodfrin <virginiegodfrin@gmail.com>
2016-07-25 14:58:15 +0200

    formulaire, entité, validator

---
+++ src/Test/BlogBundle/Validator/Antiflood.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Test\BlogBundle\Validator;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * @Annotation
+ */
+class Antiflood extends Constraint
+{
+	public $message = "Vous avez déjà posté un message il y a moins de 15 secondes, 
+  	merci d'attendre un peu.";
+
+  	public function validatedBy(){
+  		return 'test_blog_antiflood';  //appel à l'alias du service
+  	}
+
+
+}
